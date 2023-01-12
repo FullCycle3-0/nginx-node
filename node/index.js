8fbf0bc32db78288ec9e38a57d15863ba2bfaa8f
caco <cacods21@gmail.com>
2023-01-12 12:12:00 -0300

    Overall one shot commit with all the final code

--- node/index.js
+++ node/index.js
@@ -1,23 +1,64 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
-const config = {
+const mysql = require('mysql2');
+const dbConfig = {
 	'host': 'db',
 	'database': 'nodedb',
 	'user': 'root',
 	'password': 'root'
 }
-const mysql = require('mysql2');
-const conn = mysql.createConnection(config);
-const sql = `INSERT INTO people(name) values('Caco')`;
-conn.query(sql);
-conn.end();
 
-app.get('/', (req, res) => {
-	res.send('<h1>Hello Express</h1>');
-});
+const insertPeopleInDb = async(mysql, dbConfig) => {
+    const conn = mysql.createConnection(dbConfig);
+    let sql = `CREATE TABLE IF NOT EXISTS people(id INT NOT NULL auto_increment, name VARCHAR(255), PRIMARY KEY(id))`;
+    conn.query(sql);
+    sql = `INSERT INTO people (name) values ('Caco'), ('José'), ('João')`;
+    result = await conn.promise().query(sql);
+    conn.end();
+    return result;
+}
+
+const fetchPeopleFromDb = async(mysql, dbConfig) => {
+    const conn = mysql.createConnection(dbConfig);
+    const sql = `SELECT name FROM people;`;
+    const result = await conn.promise().query(sql);
+    conn.end()
+    return result[0];
+}
+
+const formatHtml = (people) => {
+    let peopleHtmlList = '';
+    people.forEach((person) => {
+        peopleHtmlList += `<li>${person.name}</li>`;
+    });
+    let html = `
+        <h1>Full Cycle Rocks!</h1>
+        <h2>Pessoas</h2>
+        <ul>
+            ${peopleHtmlList}
+        </ul>
+        `;
+    return html;
+}
 
 app.listen(port, () => {
-	console.log('Running in port ' + port);
-})
+    console.log('Running in port ' + port);
+});
+
+insertPeopleInDb(mysql, dbConfig)
+    .then(() => {
+        fetchPeopleFromDb(mysql, dbConfig)
+            .then((people) => {
+                html = formatHtml(people);
+                app.get('/', (req, res) => {
+                    res.send(html);
+                });
+            })
+            .catch((err) => {
+                    console.log(err);
+            });
+    })
+    .catch((errr) => {
+        console.log(err);
+    });
